0e642a0bac9c5ffcaf75d863eb52a5ebc94fd5fa
Amir Yasin <amiryasin3262@gmail.com>
2024-10-22 23:47:03 +0300

    Changes to be committed: 	modified:   venv/CustomUsersApp/serializers.py

--- venv/CustomUsersApp/serializers.py
+++ venv/CustomUsersApp/serializers.py
@@ -2,22 +2,21 @@ from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from .models import CustomUser
 
-# Serializer for CustomUser model
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'role']
 
 class CustomRegisterSerializer(RegisterSerializer):
-    role = serializers.CharField(max_length=20)
+    role = serializers.ChoiceField(choices=CustomUser.ROLE_CHOICES)
 
     def get_cleaned_data(self):
         data = super().get_cleaned_data()
-        data['role'] = self.validated_data.get('role') 
+        data['role'] = self.validated_data.get('role')  
         return data
 
     def create(self, validated_data):
-        user = super().create(validated_data)
-        user.role = validated_data.get('role') 
-        user.save() 
+        user = super().create(validated_data)  
+        user.role = validated_data.get('role')  
+        user.save()  
         return user
